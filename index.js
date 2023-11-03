c01fbe1081bd7f55234d77d9d6303cf595848503
minh <quangminh13032003@gmail.com>
2023-11-03 11:40:11 +0700

    change

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let images = document.querySelectorAll("img");
 for (let image of images) {
+    if (image.id == "logoimg") continue
     const viewer = new Viewer(image, {
-        toolbar:false
-    });
+        toolbar:false,
+        title: false,
+        movable: false,
+    navbar: false});
     image.addEventListener("click" , () => {
         viewer.show()
         console.log(viewer)
